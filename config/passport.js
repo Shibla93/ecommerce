35e142051f64778eca0e3e5a62aa6069ede2b80f
Fathimath Shibla <shibisabeer@gmail.com>
2026-07-27 14:48:17 +0530

    Update Google callback URL

--- config/passport.js
+++ config/passport.js
@@ -9,7 +9,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-     callbackURL: "https://ecommerce-1-vgdp.onrender.com/auth/google/callback"
+callbackURL: "https://ecommerce-2-hjyi.onrender.com/auth/google/callback"
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
